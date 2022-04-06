347e12f522aa07d97325bc68648d7830db8b14cb
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2022-04-06 02:52:35 +0200

    la aplicacion devuelve las respuestas en formato JSON

--- app/Http/Controllers/operationController.php
+++ app/Http/Controllers/operationController.php
@@ -7,35 +7,58 @@ use Illuminate\Http\Request;
 class operationController extends Controller
 {
     
-    public function operar ($operacion,$elemento1,$elemento2){
+    public function operar ($operacion,$operando1,$operando2){
         switch ($operacion) {
             case "suma":
-                return Calculator::suma($elemento1,$elemento2);   
+                return  response()->json([
+                    'operacion' => $operacion,
+                    'operando1' => $operando1,
+                    'operando2' => $operando2,
+                    'resultado' => Calculator::suma($operando1,$operando2)
+                ]);   
                 break;
             case "resta":
-                return  Calculator::resta($elemento1,$elemento2);
+                    return  response()->json([
+                    'operacion' => $operacion,
+                    'operando1' => $operando1,
+                    'operando2' => $operando2,
+                    'resultado' => Calculator::resta($operando1,$operando2)
+                ]);
                 break;
             case "multiplicacion":
-                return  Calculator::multiplicacion($elemento1,$elemento2); 
+                response()->json([
+                    'operacion' => $operacion,
+                    'operando1' => $operando1,
+                    'operando2' => $operando2,
+                    'resultado' => Calculator::multiplicacion($operando1,$operando2)
+                ]); 
                 break;
              case "division":
-                return  ($elemento2==0) ? "division por 0" :  Calculator::division($elemento1,$elemento2);
+                return  ($operando2==0) ?
+                            "division por 0" 
+                            :  
+                            response()->json([
+                                'operacion' => $operacion,
+                                'operando1' => $operando1,
+                                'operando2' => $operando2,
+                                'resultado' => Calculator::division($operando1,$operando2)
+                            ]);
                 break;
             default:
                return "la operacion no existe";
         }
     }
    /*       
-    public function suma ($elemento1,$elemento2){
-    return Calculator::suma($elemento1,$elemento2);       
+    public function suma ($operando1,$operando2){
+    return Calculator::suma($operando1,$operando2);       
     }
-    public function resta ($elemento1,$elemento2){
-    return  Calculator::resta($elemento1,$elemento2);   
+    public function resta ($operando1,$operando2){
+    return  Calculator::resta($operando1,$operando2);   
     }
-    public function multiplicacion ($elemento1,$elemento2){
-    return  Calculator::multiplicacion($elemento1,$elemento2);    
+    public function multiplicacion ($operando1,$operando2){
+    return  Calculator::multiplicacion($operando1,$operando2);    
     }
-    public function division ($elemento1,$elemento2){
-    return  ($elemento2==0) ? "division por 0" :  Calculator::division($elemento1,$elemento2);
+    public function division ($operando1,$operando2){
+    return  ($operando2==0) ? "division por 0" :  Calculator::division($operando1,$operando2);
     */
 }
